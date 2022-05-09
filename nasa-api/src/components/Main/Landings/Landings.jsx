bbe5373059d6fb05af99fac7fbddb81f30998eaa
airin181 <98608110+airin181@users.noreply.github.com>
2022-05-09 20:52:08 +0200

    components + navbar

---
+++ nasa-api/src/components/Main/Landings/Landings.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Landings = () => {
+  return <div>Landings</div>;
+};
+
+export default Landings;
---
+++ nasa-api/src/components/Main/Main.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Home from './Home/Home';
+import Neas from './Neas/Neas';
+import Landings from './Landings/Landings';
+import { Route, Routes } from 'react-router-dom';
+
+const Main = () => {
+  return <div className='main'>
+    <Routes>
+      <Route element={<Home />} path='/' />
+      <Route element={<Neas />} path='/neas' />
+      <Route element={<Landings />} path='/landings' />
+
+      {/*          <Route element={<NeasList/>} path='/landings/list'/>
+        <Route element={<LandingsList/>} path='/neas/list'/>  */}
+
+
+
+
+    </Routes>
+  </div>;
+};
+
+export default Main;
