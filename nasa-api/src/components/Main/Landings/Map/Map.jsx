1e212ae37716aa26faddca5872de412c2970a860
airin181 <98608110+airin181@users.noreply.github.com>
2022-05-12 06:31:24 +0200

    ADD: map with landings and filters. Started with FORM

--- nasa-api/src/components/Main/Landings/Map/Map.jsx
+++ nasa-api/src/components/Main/Landings/Map/Map.jsx
@@ -1,31 +1,51 @@
-import React, { useContext, useState } from "react";
-
+import React, { useContext } from "react";
+import { landingsContext } from './../../../../context/landingsContext';
 //leaflet
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
-import './react-leaflet.css';
 import Iconlocation from './IconLocation.js'
+import './react-leaflet.css';
 import 'leaflet/dist/leaflet.css';
-import { landingsContext } from './../../../../context/landingsContext';
-
 
 const Map = () => {
 
-  //landing trae un array de objetos
-  const { landing } = useContext(landingsContext)
+  //landing trae un array de objetos y 2 métodos de estado por CONTEXT
+  const {landing, setOption, setSelect, landingFilter } = useContext(landingsContext)
 
-  if (landing) {
+  
+  console.log('c.log de landingFilter en MAAPPP',landingFilter);
+
+ 
+  const handleSubmit = (event) => {  //submit del form de los filtros. Actualiza estado select que está en el padre (Landings)
+    event.preventDefault();
+    const select = event.target.by.value; 
+    const option = event.target.option.value.toUpperCase(); 
+
+    setSelect(select);
+    setOption(option);
+  }
+
+
     return <div>
 
-      <MapContainer center={[31.505, -0.09]} zoom={3} scrollWheelZoom={false}>
+      <form onSubmit={handleSubmit} className="form-filter">
+        
+        <select name="by">
+          <option value="mass" type="number" >Mass</option>
+          <option value="class" type="text">Class</option>
+        </select>
+        <input type="text" name="option" />
+        <input type="submit" />
+      </form>
+      
+
+      <MapContainer center={[31.505, -0.09]} zoom={2} scrollWheelZoom={false} >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" style={{ transform: "translate3d(-53px, -225px, 44px)" }}
-        />
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" style={{ transform: "translate3d(-53px, -225px, 44px)" }}/>
 
-        
-
-        {landing.map(element => element.reclong ?
-          (<Marker position={[element.reclat, element.reclong]} icon={Iconlocation}>
+{landing && (landingFilter === "")?
+        landing.map((element, i) => element.reclong ?
+          (<Marker position={[element.reclat, element.reclong]} icon={Iconlocation} key={i}>
             <Popup>
               <ul className="list-landings">
                 <li><b>Name: </b>{element.name}</li>
@@ -35,15 +55,39 @@ const Map = () => {
                 <li><b>Class: </b>{element.recclass}</li>
                 <li><b>Latitude: </b>{element.reclat}</li>
                 <li><b>Longitude: </b>{element.reclong}</li>
-                <li><b>Year: </b>{element.year}</li>  
+                <li><b>Year: </b>{element.year}</li>
               </ul>
             </Popup>
-          </Marker>) : null)}
+          </Marker>) : "") 
+
+           : 
+
+landing && landingFilter?
+        landingFilter.map((element, i) => element.reclat ?
+          (<Marker position={[element.reclat, element.reclong]} icon={Iconlocation} key={i}>
+            <Popup>
+
+              <ul className="list-landings">
+                <li><b>Name: </b>{element.name}</li>
+                <li><b>ID: </b>{element.id}</li>
+                <li><b>Name Type: </b>{element.nametype}</li>
+                <li><b>Mass: </b>{element.mass}kg</li>
+                <li><b>Class: </b>{element.recclass}</li>
+                <li><b>Latitude: </b>{element.reclat}</li>
+                <li><b>Longitude: </b>{element.reclong}</li>
+                <li><b>Year: </b>{element.year}</li>
+              </ul>
+            </Popup>
+          </Marker>) : console.log("landingFilter no furula")) 
+
+           : "" } 
+
 
 
       </MapContainer>
-
+ 
     </div>;
-  }
+  
 };
+
 export default Map;
